e7dd54f6d6593c49e4b99cfe9ba3565c57b9cfb9
Reyjie Mesias <reyjie.mesias@medirecords.com>
2023-02-23 23:46:36 +0800

    test

--- cypress/e2e/step-definitions/assertions/airclaim-assert-spec.ts
+++ cypress/e2e/step-definitions/assertions/airclaim-assert-spec.ts
@@ -2,7 +2,7 @@
 import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
 
-Then(/^I should be on "([^"]*)" page$/, (page:string)=>{
+Then(/^I should be on "([^"]*)" page$/, (page:string)=> {
   cy.fixture(`/element-pattern/${page.replace(/\s+/g, '')}.json`).then((selector)=>{
     const extension:string = selector.urlExtension;
     cy.url().should(`include`, extension );
